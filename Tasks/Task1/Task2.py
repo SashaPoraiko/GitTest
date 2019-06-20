6ea727354b652ae68dfe54fb9923e059547dc867
alex <poraiko.alexandr@gmail.com>
2019-06-20 15:37:12 +0300

    Task6 commit

--- Tasks/Task1/Task2.py
+++ Tasks/Task1/Task2.py
@@ -4,5 +4,5 @@ a = float(input('Enter the number A '))
 b = float(input('Enter the number B '))
 firstResult = (a + b) / 2
 secondResult = math.sqrt(a * b)
-print(firstResult,'The first result is: ')
-print(secondResult,'The second result is: ')
+print(firstResult, 'The first result is: ')
+print(secondResult, 'The second result is: ')
