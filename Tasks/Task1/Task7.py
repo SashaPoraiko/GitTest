2933a42dd6896427444b1464c5ed99b1718c6df4
alex <poraiko.alexandr@gmail.com>
2019-06-20 16:23:04 +0300

    Task 7 Commit

---
+++ Tasks/Task1/Task7.py
@@ -0,0 +1,11 @@
+import math
+
+
+# (fractions(1, 4) * math.pi)
+inside_R = 10
+inp = float(input('Enter the outside Radius: '))
+if inp > inside_R: outside_R = inp
+inside_D = inside_R * 2
+outside_D = outside_R * 2
+resultSquare = 0.07 * (outside_D ** 2 - inside_D ** 2)
+print('The resul is: ', resultSquare)
