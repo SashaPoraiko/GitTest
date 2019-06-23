d03d9952cdc4ef1157ffbe29d24d66f90622ffa0
alex <poraiko.alexandr@gmail.com>
2019-06-23 14:40:54 +0300

    Task 13 Commit

--- Tasks/Task13.py
+++ Tasks/Task13.py
@@ -1,7 +1,10 @@
+a = 1
+b = 0.1
 x = 1
-num = 0
-while num < 9:
-    x = ((x + (x / 10)) * ((x + 1) + ((x + 1) / 10)))
-    num += 1
+
+while a < 10:
+    x *= (a + b)
+    a += 1
+    b += 0.1
     print(x)
-#print(x)
+print(x)
