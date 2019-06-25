551ffa11b80ad08dd2eef9116722307c509fb448
alex <poraiko.alexandr@gmail.com>
2019-06-25 14:59:46 +0300

    Hw 1 tabs

--- Drafts/Hw_2/1.py
+++ Drafts/Hw_2/1.py
@@ -1,9 +1,12 @@
 data = input('Enter the sentence: ').split()
 greatestNumber = 0
+
 for w in data:
     if w.__len__() > greatestNumber:
         greatestNumber = w.__len__()
+
 for w in data:
     if w.__len__() == greatestNumber:
         data.remove(w)
+
 print(' '.join(data))
