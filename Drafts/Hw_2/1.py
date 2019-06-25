761f51fc413ee13006b45be00d54afddf2246e8d
alex <poraiko.alexandr@gmail.com>
2019-06-25 04:02:42 +0300

    HW 1

---
+++ Drafts/Hw_2/1.py
@@ -0,0 +1,9 @@
+data = input('Enter the sentence: ').split()
+greatestNumber = 0
+for w in data:
+    if w.__len__() > greatestNumber:
+        greatestNumber = w.__len__()
+for w in data:
+    if w.__len__() == greatestNumber:
+        data.remove(w)
+print(' '.join(data))
