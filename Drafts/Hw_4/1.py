71783f3d1c8aed19d27ff8dc47ccf0ebf909e623
alex <poraiko.alexandr@gmail.com>
2019-07-02 16:41:03 +0300

    Hw-1

---
+++ Drafts/Hw_4/1.py
@@ -0,0 +1,9 @@
+firstArr = [int(item) for item in input('Fill the first list: ').split()]
+secondArr = [int(item) for item in input('Fill the second list: ').split()]
+crossingArr = []
+
+for i in firstArr:
+    if i in secondArr and i not in crossingArr:
+        crossingArr.append(i)
+
+print('The sum is:{0}, and the greatest element is:{1}'.format(sum(crossingArr), max(crossingArr)))
