37cb459c82d255e22659b479cb1c2958c99652bb
SashaPoraiko <poraiko.alexandr@gmail.com>
2019-08-05 17:09:19 +0300

    hotfix

--- Drafts/Hw_11/1.py
+++ Drafts/Hw_11/1.py
@@ -4,11 +4,11 @@ import file_modules
 
 class Person:
     file = 'Humans.txt'
-    current_format = '%d-%m-%Y'
 
-    def __init__(self, full_name, birth_date):
+    def __init__(self, full_name, birth_date, date_format='%d-%m-%Y'):
         self.full_name = full_name
-        self.birth_date=birth_date
+        self.birth_date = birth_date
+        self.date_format = date_format
 
     def __repr__(self):
         return 'Full name of person is: {}, and the birth date is: {}'.format(self.full_name, self.birth_date)
@@ -23,13 +23,17 @@ class Person:
         return file_modules.read(self.file)
 
     def calculate_years(self):
-        return datetime.now().year - datetime.strptime(self.birth_date, '%d-%m-%Y').year
+        return datetime.now().year - datetime.strptime(self.birth_date, self.date_format).year
 
 
 saha = Person('Poraiko Oleksandr Viorelovuch', '28-06-1993')
-
+saha.save_human()
 petka = Person('Poroshenko Petro Batkovuch', '13-02-1775')
-dashka = Person('Ivanova Dasha Ivanovna', '20.20.1992')
+petka.save_human()
+dashka = Person('Ivanova Dasha Ivanovna', '20.02.1992', '%d.%m.%Y')
 dashka.save_human()
 dashka.calculate_years()
-print(dashka.get_humans())
+print(petka.get_humans())
+print(saha.calculate_years())
+print(petka.calculate_years())
+print(dashka.calculate_years())
